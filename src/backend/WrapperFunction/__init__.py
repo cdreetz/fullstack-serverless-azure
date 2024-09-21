a3d5c4aa46b0d570dc598ea2dec9c2ede8209470
Christian <cdreetz@gmail.com>
2024-09-21 02:29:57 -0500

    cors init bruv

--- src/backend/WrapperFunction/__init__.py
+++ src/backend/WrapperFunction/__init__.py
@@ -1,9 +1,18 @@
 import azure.functions as func
 
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class Item(BaseModel):
     name: str
