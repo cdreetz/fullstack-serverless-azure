359af739f069f476d20d8b8b99d1e9e940b76f9f
Christian <cdreetz@gmail.com>
2024-09-21 01:51:12 -0500

    update infra to include fastapi

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -17,6 +17,41 @@ resource "azurerm_static_site" "web" {
   location            = azurerm_resource_group.rg.location
 }
 
+# storage account
+resource "azurerm_storage_account" "sa" {
+  name                     = "mystorageaccount6969420"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_service_plan" "asp" {
+  name                = "WestUS2LinuxDynamicPlan"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  os_type             = "Linux"
+  sku_name            = "Y1"
+}
+
+# func app
+resource "azurerm_linux_function_app" "func" {
+  name                = "my-fastapi-function"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+
+  storage_account_name       = azurerm_storage_account.sa.name
+  storage_account_access_key = azurerm_storage_account.sa.primary_access_key
+  service_plan_id            = azurerm_service_plan.asp.id
+
+  site_config {
+    application_stack {
+      python_version = "3.11"
+    }
+  }
+}
+
+
 # output the default hostname of the static web app
 output "static_web_app_url" {
   value = azurerm_static_site.web.default_host_name
@@ -26,4 +61,8 @@ output "static_web_app_url" {
 output "static_web_app_api_key" {
   value     = azurerm_static_site.web.api_key
   sensitive = true
+}
+
+output "function_app_url" {
+  value = azurerm_linux_function_app.func.default_hostname
 }
